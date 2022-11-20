bc8d571de48d25d9318bdbcd82457dfcd9c5a187
claud7e <cbiver@gmail.com>
2022-11-20 10:42:27 +0100

    Add a JavaScript alert functionality

---
+++ Week 8/Homepage/index.js
@@ -0,0 +1,6 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const button = document.getElementById("btn");
+    button.addEventListener("click", () => {
+        alert("You clicked the button!");
+    });
+});
